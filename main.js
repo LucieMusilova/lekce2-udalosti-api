900903ca174576193767ee1a35dbc25f96a38745
Lucie Musilová <musil.lucie@seznam.cz>
2021-03-17 21:14:10 +0100

    Úkol je hotový

--- main.js
+++ main.js
@@ -1,1 +1,70 @@
 // tady je místo pro náš program
+
+
+let text = document.querySelector(".veta");
+
+//při najetí ztuční písmo
+
+function priNajeti() {
+  text.classList.toggle("tucne");
+}
+
+//při kliknutí text zčervená
+
+function cerveny() {
+  text.classList.toggle("red");
+}
+
+//při kliknutí se text zvětší 
+
+let aktualniVelikost = 16;
+text.style.fontSize = aktualniVelikost + "px";
+
+function zvetsi() {
+  ++aktualniVelikost;
+  text.style.fontSize = aktualniVelikost + "px";
+}
+
+//při kliknutí se spustí písnička 
+//@param {string} elementSelector
+
+function prehraj(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.play();
+}
+
+//při kliknutí se pauzne písnička 
+//@param {string} elementSelector
+
+function zastav(elementSelector) {
+    let audioElement = document.querySelector(elementSelector);
+    audioElement.pause();
+}
+
+//při kliknutí se ztiší písnička 
+
+function mute() {
+  let zvuk = document.querySelector("#zvuk");
+  zvuk.volume = 0;
+}
+
+//při kliknutí bude mít zvuk střední hlasitost
+
+function stredni() {
+  let zvuk = document.querySelector("#zvuk");
+  zvuk.volume = 0.5;
+}
+
+//při kliknutí bude mít zvuk vysokou hlasitost
+
+function zesil() {
+  let zvuk = document.querySelector("#zvuk");
+  zvuk.volume = 1;
+}
+
+//písnička začne hrát od začátku
+
+function zacatek() {
+  let zvuk = document.querySelector("#zvuk");
+  zvuk.currentTime = 0;
+}
